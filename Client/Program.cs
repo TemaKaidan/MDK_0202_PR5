1c015f783c2121951bd78401f7e53de60b560f69
Milosh <mutagarova05@inbox.ru>
2024-12-05 23:06:16 +0500

    прописали переменные, которые будут использоваться в программе клиента. Мутагарова

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Client
 {
-    internal class Program
+    class Program
     {
+        static IPAddress ServerIPAddress;
+        static int ServerPort;
+
+        static string ClientToken;
+        static DateTime ClientDateConnection;
+
         static void Main(string[] args)
         {
             
